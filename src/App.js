2a9681750d6c52202f6b4a1acdc959b34ace2aa6
samane6866 <samane.bahramian@gmail.com>
2023-07-05 11:24:41 +0200

    todo list css/done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import freecodecamplogo from "./images/800px-FreeCodeCamp_logo.png"
 
 function App() {
   return (
-    <div className="App">
-      <h1>react</h1>
+    <div className="aplicacion-tareas">
+      <div className='freecodecamp-logo-contenedor'>
+        <img src={freecodecamplogo } className='freecodecamp-logo' />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis tareas</h1>
+      </div>
     </div>
   );
 }
