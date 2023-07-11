5f1504238bf2d31185c6653655143e0ff12699a7
samane6866 <samane.bahramian@gmail.com>
2023-07-11 09:02:18 +0200

    install uuid id

--- src/components/TareaFormulario.js
+++ src/components/TareaFormulario.js
@@ -5,18 +5,21 @@ function TareaFormulario(props) {
   const [input, setInput] = useState("");
 
   const manejarCambio = (e) => {
-    console.log("escribiendo");
+    setInput(e.target.value)
+    
   };
 
-  // este funcion va ser encargada de enviar el form(tarea) cuando hacemos click a button de agregar tarea el argumento e es muy importante aqui, ya que permite  de cargar la pagina despues de enviar form
+  // este funcion va ser encargada de enviar el form(tarea) cuando hacemos click a button de agregar tarea el argumento e es muy importante aqui, ya que evita  de cargar la pagina despues de enviar form
   const manejarEnvio = (e) => {
+    e.preventDefault()
     const tareaNueva = {
       id: 3456,
-      texto: "hola",
+      texto: input,
+      completada:false
     };
   };
   return (
-    <form className="tarea-formulario">
+    <form className="tarea-formulario" onSubmit={manejarEnvio}>
       <input
         className="tarea-input"
         placeholder="escribe tu tarea"
