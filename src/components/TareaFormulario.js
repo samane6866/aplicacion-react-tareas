8b5bbf356fc96888be47f3544eb26ec1b61f3642
samane6866 <samane.bahramian@gmail.com>
2023-07-11 08:44:26 +0200

    implement onChange function

--- src/components/TareaFormulario.js
+++ src/components/TareaFormulario.js
@@ -1,12 +1,20 @@
-import React from "react";
-import "../hojas-de-estilo/Tareaformulario.css"
+import React, { useState } from "react";
+import "../hojas-de-estilo/Tareaformulario.css";
 
 function TareaFormulario(props) {
+  const [input, setInput] = useState("");
+
+  const manejarCambio = (e) => {
+    console.log("escribiendo");
+  };
 
   // este funcion va ser encargada de enviar el form(tarea) cuando hacemos click a button de agregar tarea el argumento e es muy importante aqui, ya que permite  de cargar la pagina despues de enviar form
-  const manejarEnvio = e => {
-    
-  }
+  const manejarEnvio = (e) => {
+    const tareaNueva = {
+      id: 3456,
+      texto: "hola",
+    };
+  };
   return (
     <form className="tarea-formulario">
       <input
@@ -14,6 +22,7 @@ function TareaFormulario(props) {
         placeholder="escribe tu tarea"
         type="text"
         name="texto"
+        onChange={manejarCambio}
       />
       <button className="tarea-button">Agregar tu tarea</button>
     </form>
