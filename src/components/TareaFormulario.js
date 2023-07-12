e857896333902fb00d0191a837e65f4cc4931d0f
samane6866 <samane.bahramian@gmail.com>
2023-07-12 19:55:22 +0200

    add comment

--- src/components/TareaFormulario.js
+++ src/components/TareaFormulario.js
@@ -4,12 +4,14 @@ import { v4 as uuidv4 } from "uuid";
 
 function TareaFormulario(props) {
   const [input, setInput] = useState("");
-
+// extraer el cambio de campo de input donde escribe el usario y asignamos como una nueva valor a input
   const manejarCambio = (e) => {
     setInput(e.target.value);
   };
 
   // este funcion va ser encargada de enviar el form(tarea) cuando hacemos click a button de agregar tarea el argumento e es muy importante aqui, ya que evita  de cargar la pagina despues de enviar form
+
+  // +++cada tarea debe tener un identificador unica por esta razon creamo un id unica para cada elemento, hemos installado un packete uuid npm para gener un id unica para cada tarea.
   const manejarEnvio = (e) => {
     e.preventDefault();
     const tareaNueva = {
@@ -22,6 +24,7 @@ function TareaFormulario(props) {
     props.onSubmit(tareaNueva);
   };
   return (
+    // onsubmit el evento que envia el formulario
     <form className="tarea-formulario" onSubmit={manejarEnvio}>
       <input
         className="tarea-input"
